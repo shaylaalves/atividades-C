be89c0ccbdf29ee74806dce2589f70edb41830e5
shaylaalves <142126398+shaylaalves@users.noreply.github.com>
2024-04-09 16:16:29 -0300

    Add files via upload

---
+++ quest07.c
@@ -0,0 +1,20 @@
+07)
+Algorimto calcular_S 
+
+inteiro <- N
+real <- S
+
+para i de 1 ate n faça
+	escolha i
+	   caso 3:
+	       S <- S+5
+	   caso 4:
+		S <- S+3
+	   caso 5:
+                S <- S+50
+	   caso contrario S <- S + I
+         fim escolha;
+    fimpara;
+
+       escreva("O valor de S:\n", S);
+	fim
